9aafca01682716f56c1ed6cb26c453eb4f79b873
Miso Menze <72578071+Miso-0@users.noreply.github.com>
2025-03-23 16:27:08 +0200

    Update pubspec.lock for package version upgrades and add new dependencies

--- lib/flutter_google_place_search.dart
+++ lib/flutter_google_place_search.dart
@@ -1,5 +1,6 @@
 library flutter_google_place_search;
 
+export 'package:flutter_google_place_search/src/domain/entities/location.dart';
 export 'src/domain/repositories/flutter_google_place.dart';
 export 'src/domain/entities/google_place.dart';
 export 'src/domain/entities/place_type.dart';
